8cff7c83ba00b465b1aeee67a1245386480c024b
HimanshuNankani <himanshu.nankani6@gmail.com>
2023-01-15 17:01:42 +0530

    Custom Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "./App.css";
+import Person from "./Person/Person";
 
 function App() {
   return (
-    // <div className="App">
-    //   <h1>Hi, I am a react app</h1>
-    // </div>
-    React.createElement(
-      "div",
-      { className: "App" },
-      React.createElement("h1", null, "Hi, I am a React App!!!")
-    )
+    <div className="App">
+      <h1>Hi, I am a react app</h1>
+      <p>This is really working</p>
+      <Person />
+    </div>
+    // React.createElement(
+    //   "div",
+    //   { className: "App" },
+    //   React.createElement("h1", null, "Hi, I am a React App!!!")
+    // )
   );
 }
 
